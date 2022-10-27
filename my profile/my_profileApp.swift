6be5b339c10f2cf255fcca2f4de3d070bb5c2d45
Mashael <moalharbia22@twq.idserve.net>
2022-10-27 10:01:02 +0300

    Initial Commit

---
+++ my profile/my_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_profileApp.swift
+//  my profile
+//
+//  Created by Mashael Alharbi on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct my_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
