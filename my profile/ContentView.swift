5b2636cd35c9728fe88f1f837d342ac9df4c1f02
Mashael <Messhh13@gmail.com>
2022-11-03 11:40:03 +0300

    First commit

--- my profile/ContentView.swift
+++ my profile/ContentView.swift
@@ -10,14 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("Oval")
+                .resizable()
+                .foregroundColor(Color.blue)
+                .frame(width: 150, height: 150)
+                .padding(.vertical)
+            Text("Mashael Alharbi")
+                .font(.title)
+                .foregroundColor(Color.gray)
+                .bold()
+            HStack{
+                Text("Hello, AM a student in apple devolober accademy, Am 25 years old, used to work as a driving instructour for 3years. I love cats.")
+                    .font(.headline)
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
         }
-        .padding()
+        
+        Spacer().frame(height: 30)
+        
+        VStack(alignment: .leading, spacing:20){
+            
+            }
+        }
+        }
+        
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
